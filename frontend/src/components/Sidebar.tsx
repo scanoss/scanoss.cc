c8d3f235e93e5f3262e2d63665e34df8f93ece4b
Matias Daloia <matidaloia@gmail.com>
2024-08-09 15:49:18 +0200

    feat: SP-1243 added filter by match type

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,28 +1,60 @@
 import { useQuery } from '@tanstack/react-query';
 import clsx from 'clsx';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 import { decodeFilePath, encodeFilePath } from '@/lib/utils';
+import { MatchType } from '@/modules/results/domain';
 import ResultService from '@/modules/results/infra/service';
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
 
 export default function Sidebar() {
+  const [filterByMatchType, setFilterByMatchType] = useState<MatchType | 'all'>(
+    'all'
+  );
   const { filePath } = useParams();
 
   const { data: files } = useQuery({
-    queryKey: ['results'],
-    queryFn: () => ResultService.getAll(),
+    queryKey: ['results', filterByMatchType],
+    queryFn: () =>
+      ResultService.getAll(
+        filterByMatchType === 'all' ? undefined : filterByMatchType
+      ),
   });
 
   return (
-    <div className="flex flex-col border-r bg-background h-full">
-      <h2 className="text-sm font-semibold px-4 py-6">
+    <div className="flex flex-col border-r bg-background h-full gap-4">
+      <h2 className="text-sm font-semibold px-4 pt-6">
         {files?.length
-          ? `${files.length} changes in working directory`
+          ? `${files.length} change${files.length > 1 ? 's' : ''} in working directory`
           : 'You have no changes in working directory'}
       </h2>
+      <hr />
+      <div className="px-4 flex flex-col gap-1">
+        <span className="text-xs font-semibold">Filter by match type</span>
+        <Select
+          onValueChange={(value) => setFilterByMatchType(value as MatchType)}
+          defaultValue="all"
+        >
+          <SelectTrigger className="w-full">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value={MatchType.File}>File</SelectItem>
+            <SelectItem value={MatchType.Snippet}>Snippet</SelectItem>
+            <SelectItem value={MatchType.None}>No match</SelectItem>
+            <SelectItem value="all">All</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
       <div className="flex flex-col gap-1.5">
         {files?.map((file) => {
           const isActive = decodeFilePath(filePath ?? '') === file.path;
@@ -33,14 +65,17 @@ export default function Sidebar() {
               <TooltipTrigger asChild>
                 <Link
                   className={clsx(
-                    'text-sm text-muted-foreground px-4 py-1 transition-all overflow-hidden text-ellipsis',
+                    'flex w-full items-center gap-2 text-sm text-muted-foreground px-4 py-1 transition-all',
                     isActive
                       ? 'hover:bg-primary hover:text-primary-foreground bg-primary text-primary-foreground'
                       : 'hover:bg-primary/10'
                   )}
                   to={`files/${encodedFilePath}?matchType=${file.matchType}`}
                 >
-                  {file.path}
+                  <div className="w-1.5 h-1.5 rounded-full bg-red-500"></div>
+                  <span className="overflow-hidden text-ellipsis max-w-[80%]">
+                    {file.path}
+                  </span>
                 </Link>
               </TooltipTrigger>
               <TooltipContent side="right" sideOffset={15}>
--- frontend/src/modules/results/infra/service.ts
+++ frontend/src/modules/results/infra/service.ts
@@ -1,9 +1,9 @@
 import { ResultGetAll } from '../../../../wailsjs/go/main/App';
-import { Result } from '../domain';
+import { MatchType, Result } from '../domain';
 import { mapToResult } from './mappers';
 
 export default class ResultService {
-  static async getAll(): Promise<Result[]> {
-    return ResultGetAll({}).then(mapToResult);
+  static async getAll(matchType?: MatchType): Promise<Result[]> {
+    return ResultGetAll({ matchType }).then(mapToResult);
   }
 }

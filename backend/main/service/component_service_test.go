9b131c75f8c6c7a3db5bb4179a6ad7e0c376db9c
Matias Daloia <matidaloia@gmail.com>
2024-11-11 15:48:56 +0100

    fix: Update test

--- backend/main/service/component_service_test.go
+++ backend/main/service/component_service_test.go
@@ -162,17 +162,18 @@ func TestGetDeclaredComponents(t *testing.T) {
 		assert.Len(t, declaredComponents, 3)
 		assert.Equal(t, declaredComponents, []entities.DeclaredComponent{
 			{
-				Purl: "pkg:github/purl3",
-				Name: "",
-			},
-			{
 				Purl: "pkg:npm/purl1",
 				Name: "component1",
 			},
 			{
 				Purl: "pkg:github/purl2",
 				Name: "component2",
-			}})
+			},
+			{
+				Purl: "pkg:github/purl3",
+				Name: "purl3",
+			},
+		})
 
 		resultRepo.AssertExpectations(t)
 		scanossSettingsRepo.AssertExpectations(t)

c6bc107a74b58fa3d7c0dc55f7d0e720b5535dec
Matias Daloia <matidaloia@gmail.com>
2025-03-18 07:38:28 +0100

    fix: ES-211 search bar fix

--- frontend/src/hooks/useResults.tsx
+++ frontend/src/hooks/useResults.tsx
@@ -11,23 +11,20 @@ export const useResults = () => {
   const sort = useResultsStore((state) => state.sort);
   const fetchResults = useResultsStore((state) => state.fetchResults);
 
-  const setQuery = useResultsStore((state) => state.setQuery);
-
   const debouncedQuery = useDebounce<string>(query, DEBOUNCE_QUERY_MS);
 
-  const queryResult = useQuery({
+  const { data, isLoading, refetch } = useQuery({
     queryKey: ['results', debouncedQuery, filterByMatchType, sort] as const,
     queryFn: () => fetchResults(),
   });
 
   const reset = (): void => {
-    setQuery('');
-    queryResult.refetch();
+    refetch();
   };
 
   return {
-    data: queryResult.data,
-    isLoading: queryResult.isLoading,
+    data,
+    isLoading,
     reset,
   };
 };
